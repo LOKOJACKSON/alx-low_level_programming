78dda511d4d9c08537df9985ed931a9cc01b6cd1
LOKOJACKSON <lekanadeboyega@gmail.com>
2022-06-30 21:52:37 -0700

    c programming straters

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,30 +1,20 @@
-#include <stdio.h>
-
-
-
-
+#include<unistd.h>
 
 /**
  *
- *  * main - Entry point
+ *  * main - prints the standrad error sandwiched around the  standard outpu/t
  *
  *   *
  *
- *    * Return: Always 1 (Success)
+ *    * Return: returns 1
  *
  *     */
-
-
 
 int main(void)
 
 {
 
-	fprintf(stderr, "and that piece of art is useful\" - \
-
-			Dora Korpar, 2015-10-19\n");
-
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 
